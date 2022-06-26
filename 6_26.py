85a0f187b89689147b8996de83b7b3391bd7a400
程序员乐哥 <ml2822797627@163.com>
2022-06-26 19:45:53 +0800

    基础语法

---
+++ 6_26.py
@@ -0,0 +1,67 @@
+
+'''m=22
+if m>100:
+    print('有钱')
+    print('hhhh')
+print('as')
+while 1:
+
+     print('fds')
+name = 'hello world'
+print(id(name),type(name),name)
+
+
+a = 'abc'
+b = 2
+c = 2.3
+d = True
+print(type(a),type(b),type(c),type(d))
+name = 'hello world'
+print(name.lower())
+print(name.upper())
+
+a='abc'
+b='def'
+# c=f"{a}{b}"
+# c="{}{}".format(a,b)
+print(c)
+print('effsfwef\'fwf')
+# print(3.3+3.3)
+# print(0.3+0.3)
+# print(0.1+0.2)
+name = 'maole'
+age = 32
+print(f"{name},haha")
+print(f"{name},{age}")
+# print(23/2)
+# print(23//2)
+# +加	 -减	 *乘	 /除	 //整除	 %取余	 **幂运算
+print(4+2)
+print(4-2)
+print(4*2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(2**4)
+a=True
+b=False
+print(a and b)
+print(a or b)
+c='abcd'
+d='abcdef'
+print(c in d)
+print(c not in d)
+a=3
+b=2
+print(a>>1)
+print(a<<1)
+print(a|b)
+print(a&b)
+
+age = int(input('请输入你的年龄'))
+if age<18:
+    print('未成年')
+elif 18<=age<=60:
+    print('壮年')
+else:
+    print('老年')'''
